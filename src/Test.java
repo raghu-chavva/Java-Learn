841d682020a1c386c605b64d1729a64c11c9eec3
Raghunatha Reddy <raghu.chavva2023@gmail.com>
2025-12-09 06:16:35 +0530

    Java basics

---
+++ src/Test.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Test {
+    public static void main(String[] args) {
+//        char ch='r';
+//        byte b = 120;
+//        short s= 340;
+//        long l = 1_200_000L;
+//        float f = 123.45f;
+//        double d= 123_455.78;
+//
+//        //use only Big Integer
+//        BigInteger bigInteger = new BigInteger("1234567");
+//        System.out.println(bigInteger);
+//
+//        //used mostly for float and double
+//        //float and double will loose precision so we go with Big decimal
+//
+//        BigDecimal bigDecimal = new BigDecimal("456.8999");
+//        System.out.println(bigDecimal);
+
+//        // Type Conversion
+//
+//        byte a = 127;
+//        byte b =1;
+//        byte c = (byte) (a+b);
+//        System.out.println(c); //-128
+
+//        String name1="Raghu";
+//        String name2="Raghu";
+//        System.out.println(name1==name2);
+//        System.out.println(name1.equals(name2));
+
+
+
+    }
+}
